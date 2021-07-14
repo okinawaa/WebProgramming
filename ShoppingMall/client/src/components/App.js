46b695cac14c821d6f66cd17488fde1e54cf77a4
ChanHyukPark-Tech <pgg6713@gmail.com>
2021-07-15 06:00:07 +0900

    up to delete Images using index

--- ShoppingMall/client/src/components/App.js
+++ ShoppingMall/client/src/components/App.js
@@ -7,7 +7,7 @@ import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
-
+import UploadProductPage from "./views/UploadProductPage/UploadProductPage";
 //null   Anyone Can go inside
 //true   only logged in user can go inside
 //false  logged in user can't go inside
@@ -21,6 +21,7 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/product/upload" component={Auth(UploadProductPage,true)} />
         </Switch>
       </div>
       <Footer />
---
+++ ShoppingMall/client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,92 @@
+import React, {useState} from 'react';
+import {Typography,Button,Form,message,Input,Icon} from "antd";
+import FileUpload from "../../utils/FileUpload";
+
+const {Title} = Typography;
+const {TextArea} = Input;
+
+
+const Continents = [
+    {key:1,value:"Africa"},
+    {key:2,value:"Europe"},
+    {key:3,value:"Asia"},
+    {key:4,value:"North America"},
+    {key:5,value:"South America"},
+    {key:6,value:"Australia"},
+    {key:7,value:"Antarctica"},
+]
+
+function UploadProductPage(props) {
+
+    const [TitleValue, setTitleValue] = useState("");
+    const [DescriptionValue, setDescriptionValue] = useState("");
+    const [PriceValue, setPriceValue] = useState(0);
+    const [ContinentValue, setContinentValue] = useState(1);
+
+    const [Images,setImages] = useState([]);
+
+    const onTitleChange = (event) => {
+        setTitleValue(event.currentTarget.value);
+    }
+    const onDescriptionChange = (event) => {
+        setDescriptionValue(event.currentTarget.value);
+    }
+
+    const onPriceChange = (event) =>{
+        setPriceValue(event.currentTarget.value)
+    }
+
+    const onContinentsSelectChange = (event) =>{
+        setContinentValue(event.currentTarget.value);
+    }
+
+const updateImages = (newImages) =>{
+    console.log(newImages)
+        setImages(newImages)
+}
+
+    return (
+        <div style={{maxWidth: '700px', margin: '2rem auto'}}>
+            <div style={{textAlign: 'center', marginBottom: '2rem'}}>
+                <Title level={2}> Upload Travel Product</Title>
+            </div>
+            <Form >
+                {/*Dropzone*/}
+                <FileUpload refreshFunction={updateImages}/>
+                <br/>
+                <br/>
+                <label>Title</label>
+                <Input
+                    onChange={onTitleChange}
+                    value={TitleValue}
+                />
+                <br/><br/>
+                <label>Description</label>
+                <TextArea
+                onChange={onDescriptionChange}
+                ></TextArea>
+                <br/><br/>
+                <label>Price($)</label>
+                <Input
+                    onChange={onPriceChange}
+                    type="number"
+                value={PriceValue}/>
+                <select onChange={onContinentsSelectChange}>
+
+                    {Continents.map((item,index)=>(
+                        <option key={item.key} value={item.key}>{item.value}</option>
+                    ))}
+
+                </select>
+                <br/><br/>
+
+                <Button>
+                    Submit
+                </Button>
+            </Form>
+
+        </div>
+    );
+}
+
+export default UploadProductPage;
