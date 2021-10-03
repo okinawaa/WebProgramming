218637aa821208b27d73e27260bf3a038f0046b6
Chanhyuk Park <pgg6713@gmail.com>
2021-10-03 17:07:24 +0900

    finish react-redux tutorial

--- react-redux-thunk-typescript/src/App.tsx
+++ react-redux-thunk-typescript/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {RootReducerType} from "./Store";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchPokemonData} from "./actions/PokemonActions";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [pokemonName, setPokemonName] = useState("");
+    const pokemonReducer = useSelector((state: RootReducerType) => state.PokemonReducer)
+    const dispatch = useDispatch();
+    const handlePokemonName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPokemonName(event.target.value);
+    }
+
+    const searchButtonTapped = () => {
+        dispatch(fetchPokemonData(pokemonName))
+    }
+    return (
+        <div className="App">
+            <input value={pokemonName} onChange={handlePokemonName}/>
+            <button onClick={searchButtonTapped}>포켓몬찾기</button>
+            {pokemonReducer.success && <div>
+                <p>{pokemonName}</p>
+                {pokemonReducer.pokemon?.abilities.map((ability)=>(
+                    <div>
+                        <p>{ability.ability.name}</p>
+                    </div>
+                ))}
+                <img src={pokemonReducer.pokemon?.sprites.front_default}/>
+            </div>}
+        </div>
+    )
 }
 
 export default App;
---
+++ react-redux-thunk-typescript/src/Store.ts
@@ -0,0 +1,10 @@
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from "./reducers";
+import thunk from 'redux-thunk'
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+export type RootReducerType = ReturnType<typeof rootReducer>;
+
+
+export default store;
