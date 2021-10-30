0fe08cf4eca932967cb1b701a056f10589000471
Chanhyuk Park <pgg6713@gmail.com>
2021-10-31 02:43:39 +0900

    skill height

--- portfolio/client/src/Components/Skills.js
+++ portfolio/client/src/Components/Skills.js
@@ -64,10 +64,8 @@ const SkillContainer = styled.div`
   grid-template-columns: repeat(2, 1fr);
   grid-row-gap: 2rem;
   grid-column-gap: 3rem;
-  min-height: 650px;
   @media screen and (max-width: 700px) {
     grid-template-columns: repeat(1, 1fr);
-    min-height: 1200px;
   }
 `
 
