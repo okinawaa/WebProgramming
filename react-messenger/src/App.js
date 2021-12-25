6a92583bd70cb618ff721d86ee868b42744bab46
Chanhyuk Park <pgg6713@gmail.com>
2021-12-25 23:12:24 +0900

    react-messenger init

---
+++ react-messenger/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {BrowserRouter,Switch,Route} from "react-router-dom";
+import Home from './pages/Home'
+function App() {
+  return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+        </Switch>
+      </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ react-messenger/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div>hdsfsdfsdf</div>
+    );
+}
+
+export default Home;
